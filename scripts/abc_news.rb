702293471229fcc7014769863f8be94d88af6a61
Henare Degan <henare.degan@gmail.com>
2011-08-19 15:14:27 +1000

    Now working for NSW

---
+++ scripts/abc_news.rb
@@ -0,0 +1,76 @@
+require 'rubygems'
+require 'mechanize'
+
+class ABCNews
+  def get_item(url, section)
+    agent = Mechanize.new
+    page = agent.get(url)
+    i = {}
+
+    content = page.at('div.article')
+
+    i['section'] = section
+    i['article_url'] = url
+    i['headline'] = content.at('h1').inner_text.strip
+    i['byline'] = content.at('div.byline').children.last.inner_text.strip if content.at('div.byline')
+    i['thumbnail'] = content.at('div.photo.left').at('img').attribute('src').value if content.at('div.photo.left')
+    i['article_content'] = ""
+    i['lead'] = ""
+
+    content.search('p').each_with_index do |v,k|
+      if v.attributes['class']
+        next
+      elsif k == 1
+        i['lead'] = v.inner_text
+      else
+        i['article_content'] += v.to_s
+      end
+    end
+    i['words'] = i['article_content'].split.size
+    i
+  end
+
+  def render_article(item)
+    thumbnail = item['thumbnail'] ? "<img class=\"thumbnail\" alt=\"Thumbnail\" src=\"#{item['thumbnail']}\" />" : ""
+
+    template = <<-EOF
+    <div class="section-news">
+      <div class="meta">
+        <p class="publication">The&nbsp;ABC</p>
+        <p class="section">#{item['section']}</p>
+      </div>
+      <div class="headline">
+      <h2>#{item['headline']}</h2>
+      </div>
+      <div class="intro">
+        <p class="byline">#{item['byline']}</p>
+        <p class="standfirst">#{item['lead']}</p>
+      </div>
+      <div class="body">
+        #{thumbnail}
+        #{item['article_content']}
+      </div>
+      <div class="footer">
+        <p class="original"><span>Original: </span><a href="#{item['article_url']}">#{item['article_url']}</a></p>
+      </div>
+    </div>
+    EOF
+  end
+
+  def get_headlines(url)
+    agent = Mechanize.new
+
+    base_url = "http://www.abc.net.au"
+
+    page = agent.get(url)
+
+    stories = []
+    stories << base_url + page.at("div.lead").at("a").attribute("href").value
+
+    page.at("ul.headlines").search("a").each do |a|
+      next if a.at('em') # Don't get video stories
+      stories << base_url + a.attribute("href").value
+    end
+    stories
+  end
+end
--- scripts/abc_scraper.rb
+++ scripts/abc_scraper.rb
@@ -1,59 +1,39 @@
-require 'mechanize'
+require 'abc_news'
+require 'json'
 
-def get_item(url)
-  agent = Mechanize.new
-  page = agent.get(url)
-  i = {}
+abc = ABCNews.new
 
-  content = page.at('div.article')
-  i['headline'] = content.at('h1').inner_text.strip
-  i['byline'] = content.at('div.byline').children.last.inner_text.strip
-  i['article_url'] = url
-  i['article_content'] = ""
-  i['lead'] = ""
+# sections = [
+#     {:links => [], :meta => {:url => "http://www.abc.net.au/news/nsw/", :title => "NSW"}},
+#     {:links => [], :meta => {:url => "http://www.abc.net.au/news/world/", :title => "World"}},
+#     {:links => [], :meta => {:url => "http://www.abc.net.au/news/business/", :title => "Business"}}
+# ]
+sections = [
+    {:links => [], :meta => {:url => "http://www.abc.net.au/news/nsw/", :title => "NSW"}}
+]
 
-  content.search('p').each_with_index do |v,k|
-    if v.attributes['class']
-      next
-    elsif k == 1
-      i['lead'] = v.inner_text
-    else
-      i['article_content'] += v.to_s
-    end
+sections.each do |s|
+  abc.get_headlines(s[:meta][:url]).each do |h|
+    item = abc.get_item(h, s[:meta][:title])
+    file = File.open("#{item['article_url'].split('/')[-2]}.html", 'w')
+    file.write(abc.render_article(item))
+    file.close
+    link = {
+      :path => "#{item['article_url'].split('/')[-2]}",
+      :file => "#{item['article_url'].split('/')[-2]}.html",
+      :words => item['words'],
+      :id => item['article_url'].split('/')[-2],
+      :title => item['headline']
+    }
+    s[:links] << link
   end
-
-  i['section'] = "World"
-  i['thumbnail'] = ""
-  i
 end
 
-def render_article(item)
-  thumnail = item['thumbnail'] ? "<img class=\"thumbnail\" alt=\"Thumbnail\" src=\"#{item['thumbnail']}\" />" : ""
+contents = {
+  :meta => {:max_words => 1234, :paper_name => "ABC News"},
+  :sections => sections
+}
 
-  template = <<-EOF
-  <div class="section-news">
-    <div class="meta">
-      <p class="publication">The&nbsp;ABC</p>
-      <p class="section">#{item['section']}</p>
-    </div>
-    <div class="headline">
-    <h2>#{item['headline']}</h2>
-    </div>
-    <div class="intro">
-      <p class="byline">#{item['byline']}</p>
-      <p class="standfirst">#{item['lead']}</p>
-    </div>
-    <div class="body">
-      #{item['thumbnail']}
-      #{item['article_content']}
-    </div>
-    <div class="footer">
-      <p class="original"><span>Original: </span><a href="#{item['article_url']}">#{item['article_url']}</a></p>
-    </div>
-  </div>
-  EOF
-end
-
-["http://www.abc.net.au/news/2011-08-13/sarah-palin-presidential-speculation-heats-up/2837704?section=world", "http://www.abc.net.au/news/2011-08-13/deadly-crackdown-after-friday-prayers-in-syria/2837682?section=world"].each do |url|
-  puts render_article(get_item(url))
-end
+file = File.open("contents.json", 'w')
+file.write(contents.to_json)
+file.close

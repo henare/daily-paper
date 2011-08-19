3863edfffa20020ff6783eb4ebb6f6550d791d26
Henare Degan <henare.degan@gmail.com>
2011-08-19 15:27:03 +1000

    Save the files in the correct place

--- scripts/abc_scraper.rb
+++ scripts/abc_scraper.rb
@@ -3,6 +3,9 @@ require 'json'
 
 abc = ABCNews.new
 
+file_directory = "#{File.dirname(__FILE__)}/../public/archive/#{Time.now.strftime('%Y-%m-%d')}"
+Dir.mkdir file_directory unless File.directory? file_directory
+
 # sections = [
 #     {:links => [], :meta => {:url => "http://www.abc.net.au/news/nsw/", :title => "NSW"}},
 #     {:links => [], :meta => {:url => "http://www.abc.net.au/news/world/", :title => "World"}},
@@ -15,7 +18,7 @@ sections = [
 sections.each do |s|
   abc.get_headlines(s[:meta][:url]).each do |h|
     item = abc.get_item(h, s[:meta][:title])
-    file = File.open("#{item['article_url'].split('/')[-2]}.html", 'w')
+    file = File.open("#{file_directory}/#{item['article_url'].split('/')[-2]}.html", 'w')
     file.write(abc.render_article(item))
     file.close
     link = {
@@ -34,6 +37,6 @@ contents = {
   :sections => sections
 }
 
-file = File.open("contents.json", 'w')
+file = File.open("#{file_directory}/contents.json", 'w')
 file.write(contents.to_json)
 file.close
